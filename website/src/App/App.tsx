87417055ed0b9fb7c698fe66196a97f81277c1d3
Stephan <ste.wittig@gmail.com>
2021-06-06 21:51:33 +0100

    initialise app

---
+++ website/src/App/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="title is-1">
+          Hello
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
