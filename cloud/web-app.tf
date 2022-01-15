781dbb1dd2be9406ce56cb383a81fb162038f0a1
Stephan <ste.wittig@gmail.com>
2022-01-15 22:15:33 +0000

    remove www block

--- cloud/web-app.tf
+++ cloud/web-app.tf
@@ -39,11 +39,3 @@ resource "google_app_engine_domain_mapping" "stephan_tech" {
     ssl_management_type = "AUTOMATIC"
   }
 }
-
-resource "google_app_engine_domain_mapping" "www_stephan_tech" {
-  domain_name = "www.stephan.tech"
-
-  ssl_settings {
-    ssl_management_type = "AUTOMATIC"
-  }
-}
