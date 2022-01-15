4158cfbd86e7190b18e3d9186312984ce249b0b8
Stephan <ste.wittig@gmail.com>
2022-01-15 13:53:38 +0000

    set status to terminated

--- cloud/compute.tf
+++ cloud/compute.tf
@@ -2,6 +2,8 @@ resource "google_compute_instance" "web_server" {
   name         = "web-server"
   machine_type = "e2-micro"
 
+  desired_status = "TERMINATED"
+
   boot_disk {
     initialize_params {
       image = "ubuntu-minimal-2110-impish-v20220111"
