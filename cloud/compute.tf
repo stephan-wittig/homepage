3a61203014c2267c0971cb1ecd1c5f771a49442c
Stephan <ste.wittig@gmail.com>
2022-01-15 13:25:03 +0000

    Add test TF instructions

---
+++ cloud/compute.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "web_server" {
+  name         = "web-server"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2110-impish-v20220111"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
---
+++ cloud/project.tf
@@ -0,0 +1,12 @@
+provider "google" {
+  project = "homepage-338312 "
+  region  = "us-east1"
+  zone    = "us-east1-c"
+}
+
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+
+  disable_dependent_services = true
+  disable_on_destroy = true
+}
