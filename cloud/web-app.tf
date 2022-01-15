1ccfe4f1ec7bd780434a94d86d309de10232d60f
Stephan <ste.wittig@gmail.com>
2022-01-15 20:32:50 +0000

    add deployer role

--- cloud/web-app.tf
+++ cloud/web-app.tf
@@ -1,3 +1,11 @@
 resource "google_app_engine_application" "web_app" {
   location_id = "us-east1"
 }
+
+resource "google_project_iam_binding" "cloud_builder_app_engine" {
+  role    = "roles/appengine.deployer"
+
+  members = [
+    "serviceAccount:181624353136@cloudbuild.gserviceaccount.com",
+  ]
+}
