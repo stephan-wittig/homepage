f5530f68d13aae134fd70a93340ee7c51b520eb1
Stephan <ste.wittig@gmail.com>
2022-01-18 11:52:59 +0000

    Fix spacing issues on widescreen

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const Footer: React.FC = () =>
-    <footer className="footer has-background-light">
-        <div className="footer__content">
-            <div className="block">
-                Developed by Stephan Wittig.
+    <>
+        <div className="footer__pad"></div>
+        <footer className="footer has-background-light">
+            <div className="footer__content">
+                <div className="block">
+                    Developed by Stephan Wittig.
+                </div>
             </div>
-        </div>
-    </footer>;
+        </footer>
+    </>;
 
 export default Footer;
