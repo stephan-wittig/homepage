5afa27e44d9b5ac6a281abe98022e376aa6b1ea2
Stephan <ste.wittig@gmail.com>
2022-01-19 15:13:46 +0000

    Add responsive CV content

--- website/src/screens/Cv/Screen.tsx
+++ website/src/screens/Cv/Screen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { LinkedInLink } from "../../components";
+import Segment from "./Segment";
 
 const Cv: React.FC = () =>
     <>
@@ -14,15 +14,44 @@ const Cv: React.FC = () =>
                         contact me
                     </Link>. 
                 </p>
-                <p className="block">
-                    <div className="message is-warning">
-                        <div className="message-body">
-                        I will add my CV here soon-ish. Most of it is already on LinkedIn™: <LinkedInLink>stephan-wittig</LinkedInLink>
-                        </div>
-                    </div>
-                </p>
             </div>
         </section>
+        <Segment title="Frankfurt School and Deutsche Bank" dateRange={["Aug 2015", "Jul 2019"]}>
+            <div>
+                A &quot;Duales Studium&quot; is a university programme accompanied by practical work.
+                In my case, I studied Information Systems at <em>Frankfurt School of Finance &amp; Management</em> and worked in the IT department of <em>Deutsche Bank</em>'s retail bank.
+            </div>
+            <div>
+                In Deutsche Bank's office in Eschborn (near Frankfurt) I received training as a <em>Computer Science Expert</em> apprentice.
+                In this two-year programme, I worked in multiple roles of the software development cycle and gathered experience in requirements analysis, programming, testing, IT architecture and project management.
+            </div>
+            <div>
+                I also chose to undertake two placements, one in business architecture in Frankfurt, and the other one in the chief digital office in London.
+            </div>
+            <div>
+                After graduating as apprentice with the highest possible grade in 2017, I continued working as Business Analyst.
+                In multiple projects, I analysed business processes and requirements, especially in the area of data governance and data protection.
+                I also supported our project management and managed smaller projects myself.
+            </div>
+            <div>
+                At Frankfurt School of Finance &amp; Management &hellip;
+            </div>
+        </Segment>
+        <Segment title="London and Breaking Wave" dateRange={["Aug 2019", "Sep 2021"]}>
+            <div>
+                I moved to London to work at Breaking Wave, a subsidiary of Deutsche Bank that works with new technologies using agile rapid-prototyping approaches.
+                I joined just after this new team was created, first as an engineer seconded from Deutsche Bank.
+                From Jan 2020, I was directly employed by Breaking Wave.
+                &hellip;
+            </div>
+        </Segment>
+        <Segment title="London School of Economics" dateRange={["Sep 2021", "Aug 2022"]}>
+            <div>
+                Having learned a lot about technology as an engineer, I decided to learn more about its social aspects.
+                That is why I went back to university for an MSc in Management of Information Systems and Digital Innovation.
+                &hellip;
+            </div>
+        </Segment>
     </>;
 
 export default Cv;
