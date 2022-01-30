53a5706f21f2f3df7e05eeef5bfec355a94c55df
Stephan <ste.wittig@gmail.com>
2022-01-30 21:05:11 +0000

    use correct payload

--- website/src/screens/Contact.tsx
+++ website/src/screens/Contact.tsx
@@ -9,18 +9,10 @@ const Contact: React.FC = () =>
                     Contact me
                 </h2>
                 <div className="block">
-                    If you use it you can connect with me on LinkedIn™: <LinkedInLink>stephan-wittig</LinkedInLink>
+                    You can connect with me on LinkedIn™: <LinkedInLink>stephan-wittig</LinkedInLink>
                 </div>
                 <div className="block">
                     Or you could use the following form to send me an email. Generally, I try to answer within a few days.
-                </div>
-            </div>
-            <div className="block container is-max-desktop">
-                <div className="message is-warning">
-                    <div className="message-body">
-                        The form for sending emails is currently under construction. 
-                        Please return later or use LinkedIn to contact me. Thank you for your interest!
-                    </div>
                 </div>
             </div>
             <div className="container block is-max-desktop">
