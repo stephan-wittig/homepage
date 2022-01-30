834a753a2927f60d67e5167c5369ad3495e95288
Stephan <ste.wittig@gmail.com>
2022-01-30 20:35:59 +0000

    add send functionality

--- api/connectors/email.go
+++ api/connectors/email.go
@@ -3,12 +3,14 @@ package connectors
 import (
 	"context"
 	"fmt"
+	"net/smtp"
 
 	"github.com/stephan-wittig/homepage/api/models"
 )
 
 type EmailClient struct {
 	host     string
+	port     string
 	username string
 	password string
 	from     string
@@ -24,6 +26,7 @@ func NewEmailClient() (EmailClient, error) {
 
 	config, err := sm.GetSecrets(ctx, []string{
 		"EMAIL_HOST",
+		"EMAIL_PORT",
 		"EMAIL_USERNAME",
 		"EMAIL_PASSWORD",
 		"EMAIL_FROM",
@@ -33,10 +36,9 @@ func NewEmailClient() (EmailClient, error) {
 		return EmailClient{}, err
 	}
 
-	fmt.Printf("Got all the secrets, eg EMAIL_USERNAME: '%s'\n", config["EMAIL_USERNAME"])
-
 	return EmailClient{
 		host:     config["EMAIL_HOST"],
+		port:     config["EMAIL_PORT"],
 		username: config["EMAIL_USERNAME"],
 		password: config["EMAIL_PASSWORD"],
 		from:     config["EMAIL_FROM"],
@@ -45,6 +47,25 @@ func NewEmailClient() (EmailClient, error) {
 }
 
 func (ec *EmailClient) SendEmail(m models.Message) error {
-	fmt.Printf("%+v", m)
-	return nil
+	auth := smtp.PlainAuth("", ec.username, ec.password, ec.host)
+
+	msg := fmt.Sprintf("To: %s\r\n", ec.to) +
+		fmt.Sprintf("From: %s\r\n", ec.from) +
+		fmt.Sprintf("Subject: %s\r\n", m.Subject) +
+		fmt.Sprintf("Reply-To: %s\r\n", m.SenderEmail) +
+		"\r\n" +
+		fmt.Sprintf("The following message was submitted by '%s' on stephan.tech/contact\r\n", m.SenderEmail) +
+		"\r\n" +
+		fmt.Sprintf("Subject: '%s'\r\n", m.Subject) +
+		"\r\n" +
+		fmt.Sprintf("%s\r\n", m.Content)
+
+	err := smtp.SendMail(
+		fmt.Sprintf("%s:%s", ec.host, ec.port),
+		auth,
+		ec.from,
+		[]string{ec.to},
+		[]byte(msg),
+	)
+	return err
 }
