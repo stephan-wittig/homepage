82f9a1ab4504c73bc03003ef3db93003b9bcc667
Stephan Wittig <42174878+stephan-wittig@users.noreply.github.com>
2022-01-31 01:47:55 +0000

    Feature/refactor tf (#57)
    * make tf smarter
    
    * fmt plan
    
    * typo
    
    * change appengine storage location
    
    * move block

--- cloud/app-engine.tf
+++ cloud/app-engine.tf
@@ -2,34 +2,15 @@ resource "google_app_engine_application" "web_app" {
   location_id = "us-east1"
 }
 
-resource "google_project_iam_binding" "cloud_builder_app_engine" {
-  project = "homepage-338312"
-  role    = "roles/appengine.appAdmin"
-
-  members = [
-    "serviceAccount:181624353136@cloudbuild.gserviceaccount.com",
-  ]
-}
-
-resource "google_project_iam_binding" "cloud_builder_storage" {
-  project = "homepage-338312"
-  role    = "roles/storage.objectAdmin"
-
-  members = [
-    "serviceAccount:181624353136@cloudbuild.gserviceaccount.com",
-  ]
-}
-
 data "google_app_engine_default_service_account" "default" {
 }
 
-resource "google_service_account_iam_binding" "admin-account-iam" {
-  service_account_id = data.google_app_engine_default_service_account.default.name
-  role               = "roles/iam.serviceAccountUser"
-
-  members = [
-    "serviceAccount:181624353136@cloudbuild.gserviceaccount.com",
-  ]
+resource "google_storage_bucket" "appengine" {
+  name          = "us.artifacts.${var.project_id}.appspot.com "
+  location      = "US-EAST1"
+  
+  force_destroy = true
+  uniform_bucket_level_access = true
 }
 
 resource "google_app_engine_domain_mapping" "stephan_tech" {
@@ -38,4 +19,31 @@ resource "google_app_engine_domain_mapping" "stephan_tech" {
   ssl_settings {
     ssl_management_type = "AUTOMATIC"
   }
+}
+
+resource "google_project_iam_binding" "cloud_builder_app_engine" {
+  project = var.project_id
+  role    = "roles/appengine.appAdmin"
+
+  members = [
+    "serviceAccount:${local.cloudbuild_sa}",
+  ]
+}
+
+resource "google_project_iam_binding" "cloud_builder_storage" {
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+
+  members = [
+    "serviceAccount:${local.cloudbuild_sa}",
+  ]
+}
+
+resource "google_service_account_iam_binding" "cloud_builder_appengine_sa" {
+  service_account_id = data.google_app_engine_default_service_account.default.name
+  role               = "roles/iam.serviceAccountUser"
+
+  members = [
+    "serviceAccount:${local.cloudbuild_sa}",
+  ]
 }
